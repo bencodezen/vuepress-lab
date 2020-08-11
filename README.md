634f1f0a1f02fafd593445181726c9f836579ab7
Ben Hong <ben@bencodezen.io>
2020-08-11 17:00:05 -0400

    feature: setup project

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# vuepress-sandbox
+# VuePress Sandbox
+
+Playground for VuePress development.
+
+## Getting Started
+
+```sh
+# Install dependencies
+yarn
+
+# Start local env
+yarn dev
+```
---
+++ docs/.vuepress/config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  title: 'VuePress Site Title',
+  description: 'This is your landing page description.',
+  themeConfig: {
+    logo: '/vuepress-logo.png',
+    lastUpdated: 'Last updated',
+    nav: [
+      {
+        text: 'Home',
+        link: '/'
+      },
+      {
+        text: 'Basic Page',
+        link: '/basic'
+      },
+      {
+        text: 'Section',
+        items: [
+          {
+            text: 'Section Introduction',
+            link: '/section/#section-introduction'
+          },
+          {
+            text: 'Some More Content!',
+            link: '/section/#some-more-content'
+          }
+        ]
+      }
+    ],
+    plugins: ['@vuepress/active-header-links']
+  }
+};
